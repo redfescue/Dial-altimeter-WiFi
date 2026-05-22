27e226e9ef4dc6f35fb43014998194f0fbe1fcea
Steve Hall <shall317@yahoo.com>
2026-05-22 16:38:22 -0700

    Add secrets.h template for WiFi and API credentials

---
+++ secrets.h
@@ -0,0 +1,11 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi credentials
+#define WIFI_SSID "your_ssid_here"
+#define WIFI_PASSWORD "your_password_here"
+
+// API credentials
+#define API_KEY "your_api_key_here"
+
+#endif
